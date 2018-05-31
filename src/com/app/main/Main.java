13a8e8cf9211d13326de08f94722f2297340f003
Prasad108 <prasad.dukale@atos.net>
2018-05-31 13:56:49 +0530

    initial commit

---
+++ src/com/app/main/Main.java
@@ -0,0 +1,13 @@
+package com.app.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello ");
+		for(String arg:args)
+		System.out.println(arg);
+
+	}
+
+}
