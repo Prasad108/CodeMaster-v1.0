96d4d33108ed99bf6bcd2b4f9e0c5abfdabc01da
A665792 <rohan.diwate@atos.net>
2018-05-31 16:43:56 +0530

    Solution

---
+++ Problem2/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int tests = 0;
+	cin>>tests;
+	for(int i=0;i<tests;i++)
+	{
+		unsigned int value = 0x01;
+		unsigned int clappers = 0;
+		unsigned int claps = 0;
+		unsigned int temp = 0;
+		cin>>clappers>>claps;
+		for(int i = 0; i<clappers;i++)
+		{
+			temp |= value;
+			value<<1;
+		}
+		cout<<"Experiment-"<<i+1<<": ";
+		if (claps & temp == temp)
+			cout<<"ON"<<endl;
+		else
+			cout<<"OFF"<<endl;
+	}
+
+	return 0;
+}
